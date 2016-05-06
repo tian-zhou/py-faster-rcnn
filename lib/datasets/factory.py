c41725df8342579340f0001f133d920ae4174d9f
Tian Zhou <leochou1991@gmail.com>
2016-05-06 11:51:45 -0700

    Update factory.py

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -11,6 +11,8 @@ __sets = {}
 
 from datasets.pascal_voc import pascal_voc
 from datasets.coco import coco
+from datasets.inria import inria
+
 import numpy as np
 
 # Set up voc_<year>_<split> using selective search "fast" mode
@@ -30,6 +32,12 @@ for year in ['2015']:
     for split in ['test', 'test-dev']:
         name = 'coco_{}_{}'.format(year, split)
         __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+inria_devkit_path = '/home/tzhou/Software/py-faster-rcnn/data/inria'
+for split in ['train', 'valid', 'test', 'trainvalid']:
+    name = '{}_{}'.format('inria', split)
+    __sets[name] = (lambda split=split: inria(split, inria_devkit_path))
+
 
 def get_imdb(name):
     """Get an imdb (image database) by name."""
